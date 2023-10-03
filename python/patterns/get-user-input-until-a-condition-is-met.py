d21844fe45b3c6571ab1418359e01731044f9f7b
Justin Wyllie <justinwyllie@hotmail.co.uk>
2023-10-03 16:51:05 +0300

    Examples of getting user input until a condition is met

---
+++ python/patterns/get-user-input-until-a-condition-is-met.py
@@ -0,0 +1,23 @@
+#example one - only accept digits
+n1 = ''
+
+while not n1.isdigit():
+    print("Enter only numbers please")
+    n1 = input("Type a number: ")
+   
+print("You entered", n1)
+
+#example two - keep getting user data until user enters a certain char (and fill a list)
+shoppingList = [] #a list
+
+print("Enter shopping list items and X when finished")
+item = ''
+
+while item != 'X':
+    item = input("Enter name: ") 
+    #do not enter 'X'
+    if item != "X":
+        shoppingList.append(item)
+
+print("Shopping list", shoppingList)
+
