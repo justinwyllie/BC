0943444183ab8d8e1fbd1b574370a0481796794e
Justin Wyllie <justinwyllie@hotmail.co.uk>
2024-02-13 15:20:26 +0300

    More selective about data

--- html/ajax-handler2.php
+++ html/ajax-handler2.php
@@ -6,11 +6,23 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $data["errors"] = implode("-",$data["errors"]);
 
-$email_data = <<<EOT
-name: {$data["name"]}
-tea: {$data["tea"]}
-coffee: {$data["coffee"]}
-EOT;
+$email_data = "";
+
+$email_data = "name: " . $data["name"];
+
+if ($data["tea"] != "Select..")
+{
+    $email_data = $email_data . "tea: " . $data["tea"];
+}
+
+if ($data["coffee"] != "Select..")
+{
+    $email_data = $email_data . "coffee " . $data["coffee"];
+}
+
+
+
+
 
 $res = mail('justinwyllie@hotmail.co.uk', "Menu Order", $email_data);
 echo json_encode(array('success' => $res));
